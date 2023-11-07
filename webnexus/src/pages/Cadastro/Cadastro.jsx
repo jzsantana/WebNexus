16038d6fea3f33a36d61fca65284ea10db1ba626
jzsantana <juliass7187@gmail.com>
2023-11-07 11:07:37 -0300

    atualizando

--- webnexus/src/pages/Cadastro/Cadastro.jsx
+++ webnexus/src/pages/Cadastro/Cadastro.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
 import './Cadastro.css'
+import Logo from '../../assets/Logos/nexus_logo_banner.svg'
 
 const Cadastro = () => {
   return (
-    <div>Cadastro</div>
+    <div className="cadastro">
+        <div className="container-principal-cadastro">
+            <div className="modal-cadastro">
+                <div className="image-div-cad"></div>
+                <div className="cadastro-info-div">
+                    <div className="cad-div-logo">
+                        <img src={Logo} alt="" />
+                    </div>
+
+                    <div className="input-nome">
+                        <input type="text" />
+                        <span>Nome completo</span>
+                    </div>
+
+                    <div className="input-cpf">
+                        <input type="text" />
+                        <span>CPF</span>
+                    </div>
+
+                    <div className="input-email">
+                        <input type="text" />
+                        <span>Email</span>
+                    </div>
+
+                    <div className="input-nascimento">
+
+                        <span>Data de nascimento</span>
+                    </div>
+
+                    <div className="div-endereco-info">
+                        <div className="input-cep">
+                            <input type="text" />
+                            <span>CEP</span>
+                        </div>
+
+                        <div className="input-cidade">
+                            <input type="text" />
+                            <span>Cidade</span>
+                        </div>
+                    </div>
+                        
+
+                    <div className="input-estado">
+                        <input type="text" />
+                        <span>Estado</span>
+                    </div>
+
+                    <div className="input-endereco">
+                        <input type="text" />
+                        <span>Endereço</span>
+                    </div>
+
+                    <div className="input-bairro">
+                        <input type="text" />
+                        <span>Bairro</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- webnexus/src/pages/Login/Login.jsx
+++ webnexus/src/pages/Login/Login.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../components/Navbar/Navbar'
 
 import { FaBuildingUser, FaUserLarge, FaEyeSlash} from 'react-icons/fa6'
 import Logo from '../../assets/Logos/nexus_logo_banner.svg'
+import Porquinho from '../../assets/Piggy.svg'
 
 import './Login.css'
 
@@ -12,33 +13,29 @@ const Login = () => {
         {/* <Navbar/> */}
         <div className="container-principal-login">
             <div className='modal-login'>
-                <div className='image-div-login'></div>
+                <div className='image-div-login'>
+                    <h1>Bem-vindo <br></br>de volta!</h1>
+                </div>
                 
                 <div className='login-info-div'>
-
                     <div className='login-div-logo'>
                         <img src={Logo} alt="" />
                     </div>
-
                     <div className='div-input-agencia'>
                         <span><FaBuildingUser color='#7088DD' size={24}/></span>
                         <input type="text" placeholder='Insira o número de sua agência' />
                     </div>
-
                     <div className='div-input-conta'>
                         <span><FaUserLarge color='#7088DD' size={24}/></span>
                         <input type="text" placeholder='Insira o número de sua conta'/>
                     </div>
-
                     <div className='div-input-senha'>
                         <span><FaEyeSlash color='#7088DD' size={24}/></span>
                         <input type="password" placeholder='Insira sua senha'/>
                     </div>
-
                     <p className='forgot-password'>Esqueceu a senha?</p>
                     <button className="btn-info-login">LOGIN</button>
                 </div>
-
             </div>
         </div>
     </div>
